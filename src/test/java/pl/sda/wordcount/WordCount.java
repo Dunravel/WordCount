c3e1de20ad5e19736810fb5447a398e8c0ee3c69
Marcin Iskra <dunravel@gmail.com>
2018-10-20 14:07:06 +0200

    WordCount - Implemented return 0 if word not added

--- src/test/java/pl/sda/wordcount/WordCount.java
+++ src/test/java/pl/sda/wordcount/WordCount.java
@@ -1,7 +1,15 @@
 package pl.sda.wordcount;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
+    Map<String,Integer> wordMap = new HashMap<>();
+
     public int count(String word){
-        return 0;
+        if(!wordMap.containsKey(word)) {
+            return 0;
+        }
+        return -1;
     }
 }
