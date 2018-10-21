ffaa01731c50a7eee861a6b00c49a825d375d7c1
Marcin Iskra <dunravel@gmail.com>
2018-10-21 14:16:32 +0200

    TestSuite - TEst scenario by Author is added when is test scenario is correct and author has privileges

--- src/main/java/pl/sda/testsuite/TestSuite.java
+++ src/main/java/pl/sda/testsuite/TestSuite.java
@@ -3,13 +3,17 @@ package pl.sda.testsuite;
 import java.util.Map;
 
 class TestSuite {
-    private TestScenarioValidator validator;
+    private final TestScenarioValidator validator;
+    private AuthorAccess authorAccess;
+    private Map<Author,TestScenario> testScenarios;
 
     TestSuite(TestScenarioValidator validator, AuthorAccess authorAccess, Map<Author, TestScenario> testScenarios){
         this.validator = validator;
+        this.authorAccess = authorAccess;
+        this.testScenarios = testScenarios;
     }
 
     void add(TestScenario testScenario, Author author) {
-
+        testScenarios.put(author,testScenario);
     }
 }
