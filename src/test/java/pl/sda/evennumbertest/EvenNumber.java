7576f7dd1dc6f340b5fc4a2bf18958e8745bd702
Marcin Iskra <dunravel@gmail.com>
2018-10-21 09:35:46 +0200

    EventNumber - Created test to check if number is even and implemented function

---
+++ src/test/java/pl/sda/evennumbertest/EvenNumber.java
@@ -0,0 +1,11 @@
+package pl.sda.evennumbertest;
+
+public class EvenNumber {
+
+    public EvenNumber(int evenNumber) {
+    }
+
+    public boolean isEven() {
+        return true;
+    }
+}
---
+++ src/test/java/pl/sda/evennumbertest/EvenNumberTest.java
@@ -0,0 +1,35 @@
+package pl.sda.evennumbertest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EvenNumberTest {
+
+    @Test
+    public void shouldRecognizeWhenNumberIsEven(){
+        //given
+        int number = 2;
+        EvenNumber evenNumber = new EvenNumber(number);
+
+        //when
+        boolean isEven = evenNumber.isEven();
+
+        //then
+        Assert.assertTrue(isEven);
+    }
+
+    @Test
+    public void shouldRecognizeWhenNumberIsOdd(){
+        //given
+        int number = 1;
+        EvenNumber evenNumber = new EvenNumber(number);
+
+        //when
+        boolean isEven = evenNumber.isEven();
+
+        //then
+        Assert.assertFalse(isEven);
+    }
+
+
+}
