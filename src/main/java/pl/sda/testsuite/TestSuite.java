c161cc54744e3af24f18829fd5d200b9a7564001
Marcin Iskra <dunravel@gmail.com>
2018-10-21 14:45:36 +0200

    TestSuite - Implement: not add if test scenario is not correct

--- src/main/java/pl/sda/testsuite/TestSuite.java
+++ src/main/java/pl/sda/testsuite/TestSuite.java
@@ -5,14 +5,17 @@ import java.util.Map;
 class TestSuite {
     private final Map<Author,TestScenario> testScenarios;
     private final AuthorAccess authorAccess;
+    private final TestScenarioValidator validator;
 
     TestSuite(TestScenarioValidator validator, AuthorAccess authorAccess, Map<Author, TestScenario> testScenarios){
+        this.validator = validator;
         this.authorAccess = authorAccess;
         this.testScenarios = testScenarios;
+
     }
 
     void add(TestScenario testScenario, Author author) {
-        if(authorAccess.hasPrivileges(author)) {
+        if(authorAccess.hasPrivileges(author) && validator.isCorrect(testScenario)) {
             testScenarios.put(author, testScenario);
         }
     }
