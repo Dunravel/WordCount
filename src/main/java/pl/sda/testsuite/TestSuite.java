4916b5bd420ef6cca5513b4c2bff1d494f0f53ce
Marcin Iskra <dunravel@gmail.com>
2018-10-21 14:18:24 +0200

    TestSuite - Refactor: removed unused fields

--- src/main/java/pl/sda/testsuite/TestSuite.java
+++ src/main/java/pl/sda/testsuite/TestSuite.java
@@ -3,13 +3,9 @@ package pl.sda.testsuite;
 import java.util.Map;
 
 class TestSuite {
-    private final TestScenarioValidator validator;
-    private AuthorAccess authorAccess;
     private Map<Author,TestScenario> testScenarios;
 
     TestSuite(TestScenarioValidator validator, AuthorAccess authorAccess, Map<Author, TestScenario> testScenarios){
-        this.validator = validator;
-        this.authorAccess = authorAccess;
         this.testScenarios = testScenarios;
     }
 
